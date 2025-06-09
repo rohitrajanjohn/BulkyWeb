83ed01b537cb6b802a5bc22824f67e71bb1ffe74
Rohit <rohit.r@ainsurtek.com>
2025-06-09 18:00:56 +0530

    Category Controller Created

---
+++ BulkyWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+		{
+			
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
